8e5096ca3992b9d56c0fa94184344379d06f1340
INTELIGENCIA <darodriguesufc@gmail.com>
2024-10-10 11:48:17 -0300

    OI

--- src/app/pages/page-outlet/components/async-pipe/async-pipe.component.ts
+++ src/app/pages/page-outlet/components/async-pipe/async-pipe.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { City, GlobalService } from '../../../../services/GlobalService';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-async-pipe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers:[GlobalService],
   templateUrl: './async-pipe.component.html',
   styleUrl: './async-pipe.component.css'
 })
 export class AsyncPipeComponent {
+  public $cities!:Observable<City[]>
+
+  constructor(private globalService:GlobalService){
+    this.$cities = globalService.city$;
+    this.globalService.getCities();
+  }
 
 }
